d5024e795c2018eb997b8e91a30319874bb5f790
Elihyv <jeanharoviu@gmail.com>
2026-03-06 14:41:49 -0600

    vue1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Añadimos la base con el nombre exacto de tu repositorio en GitHub
+  base: '/vite-project/',
 })
